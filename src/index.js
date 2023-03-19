ae69bb5abda7d94eeb5c6b9603474f7a94cfddfa
OrdinarilySam <56322367+OrdinarilySam@users.noreply.github.com>
2023-03-19 01:19:23 -0500

    Added welcome message

--- src/index.js
+++ src/index.js
@@ -14,4 +14,21 @@ bot.on('ready', (client) => {
     console.log(`${client.user.tag} is online.`)
 })
 
+bot.on('guildMemberAdd', (member) => {
+    console.log(`${member.user.tag} joined the server.`)
+    bot.channels.fetch(member.guild.systemChannelId)
+        .then(channel => channel.send(`${member.user} joined the server.`))
+
+
+})
+
+bot.on('messageCreate', message => {
+    if(message.author.bot){
+        return
+    }
+    if(message.content == "purge"){
+        message.channel.bulkDelete(100)
+    }
+})
+
 bot.login(process.env.TOKEN);
